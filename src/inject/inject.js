2d8528a38adb60034e7205beeaf2c13dde1ea1fa
Eric Thong <ethong@cs.stanford.edu>
2015-02-16 01:10:14 -0800

    Add files

---
+++ src/inject/inject.js
@@ -0,0 +1,54 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 erkserkserks
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// Override video element canPlayType() function
+var videoElem = document.createElement('video');
+var origCanPlayType = videoElem.canPlayType.bind(videoElem);
+videoElem.__proto__.canPlayType = function(type) {
+  if (type === undefined) return '';
+  // If queried about webM/vp8/vp8 support, say we don't support them
+  if (type.indexOf('webm') != -1 ||
+      type.indexOf('vp8') != -1 ||
+      type.indexOf('vp9') != -1) {
+    return '';
+  }
+  // Otherwise, ask the browser
+  return origCanPlayType(type);
+}
+
+// Override media source extension isTypeSupported() function
+var mse = window.MediaSource;
+var origIsTypeSupported = mse.isTypeSupported.bind(mse);
+mse.isTypeSupported = function(type) {
+  if (type === undefined) return '';
+  // If queried about webM/vp8/vp8 support, say we don't support them
+  if (type.indexOf('webm') != -1 ||
+      type.indexOf('vp8') != -1 ||
+      type.indexOf('vp9') != -1) {
+    return '';
+  }
+  // Otherwise, ask the browser
+  return origIsTypeSupported(type);
+}
+
