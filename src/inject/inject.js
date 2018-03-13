49b2bae4b4893a6713e18bb6ef1c611ae09391a8
Eric Thong <thongerick@gmail.com>
2018-03-13 00:00:21 -0400

    Add localization to battery only option.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -27,6 +27,30 @@ function inject () {
     return;
   }
 
+  if (localStorage['h264ify-battery_only'] && navigator.getBattery) {
+    navigator.getBattery().then(function(battery) {
+      if (!battery.charging) {
+        override();
+      }
+    })
+  } else {
+    override();
+  }
+
+  function override() {
+    // Override video element canPlayType() function
+    var videoElem = document.createElement('video');
+    var origCanPlayType = videoElem.canPlayType.bind(videoElem);
+    videoElem.__proto__.canPlayType = makeModifiedTypeChecker(origCanPlayType);
+
+    // Override media source extension isTypeSupported() function
+    var mse = window.MediaSource;
+    // Check for MSE support before use
+    if (mse === undefined) return;
+    var origIsTypeSupported = mse.isTypeSupported.bind(mse);
+    mse.isTypeSupported = makeModifiedTypeChecker(origIsTypeSupported);
+  }
+
   // return a custom MIME type checker that can defer to the original function
   function makeModifiedTypeChecker(origChecker) {
     // Check if a video type is allowed
@@ -46,30 +70,6 @@ function inject () {
       // Otherwise, ask the browser
       return origChecker(type);
     };
-  }
-
-  function override() {
-    // Override video element canPlayType() function
-    var videoElem = document.createElement('video');
-    var origCanPlayType = videoElem.canPlayType.bind(videoElem);
-    videoElem.__proto__.canPlayType = makeModifiedTypeChecker(origCanPlayType);
-
-    // Override media source extension isTypeSupported() function
-    var mse = window.MediaSource;
-    // Check for MSE support before use
-    if (mse === undefined) return;
-    var origIsTypeSupported = mse.isTypeSupported.bind(mse);
-    mse.isTypeSupported = makeModifiedTypeChecker(origIsTypeSupported);
-  }
-
-  if (localStorage['h264ify-battery_only'] && navigator.getBattery) {
-    navigator.getBattery().then(function(battery) {
-      if (!battery.charging) {
-        override();
-      }
-    })
-  } else {
-    override();
   }
 }
 
